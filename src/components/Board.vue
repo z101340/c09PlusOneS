7f4bc7e39ba53fd18dd1d4a971a80106b8d4f231
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2019-03-05 22:25:26 -0800

    add board and matrix

---
+++ src/components/Board.vue
@@ -0,0 +1,36 @@
+<template>
+    <Matrix v-bind:matrix="matrix" v-bind:HEIGHT="HEIGHT" v-bind:WIDTH="WIDTH" />
+</template>
+
+<script>
+import Matrix from './Matrix.vue'
+
+export default {
+  name: "board",
+  components: {
+    Matrix
+  },
+  data() {
+    return {
+      WIDTH: 10,
+      HEIGHT: 20,
+      matrix: []
+    }
+  },
+
+  methods: {
+    initMatrix: function() {
+      for (let row = 0; row < this.HEIGHT; row++) {
+          this.matrix.push([])
+          for (let column = 0; column < this.WIDTH; column++) {
+              this.matrix[row].push(0)
+          }
+      }
+    }
+  },
+
+  created() {
+    this.initMatrix()
+  }
+}
+</script>
---
+++ src/components/Matrix.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="grid-container">
+        <div v-for="row in matrix" class="row">
+            <div v-for="cell in row" class="cell">
+                {{ cell }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'HEIGHT',
+        'WIDTH',
+        'matrix'
+    ]
+}
+</script>
+
+<style scoped>
+.row{
+  display: flex;
+}
+.row div{
+  width: 20px;
+  height: 20px;
+  border: 1px solid black;
+}
+</style>
