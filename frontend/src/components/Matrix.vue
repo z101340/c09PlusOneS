4fe66683719c3fe654c8d69217d9ea196edb0bcb
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2019-03-23 19:23:44 -0700

    move frontend code to frontend folder

---
+++ frontend/src/components/Matrix.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="grid-container">
+        <div v-for="row in matrix" class="row">
+            <div v-for="cell in row" v-bind:class="{ black: cell }" class="cell">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'matrix'
+    ]
+}
+</script>
+
+<style scoped>
+.grid-container {
+    width: fit-content;
+}
+.row{
+  display: flex;
+}
+.row div{
+  width: 20px;
+  height: 20px;
+  /* border: 0.5px solid rgb(184, 184, 184); */
+  box-shadow: 0 1px 1px 0 rgba(0,0,0,0.2);
+}
+.black {
+    background-color: black;
+}
+</style>
---
+++ frontend/src/components/Side.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="card next">
+            <header class="card-header">
+                <p class="card-header-title">
+                    Next
+                </p>
+            </header>
+            <NextTetromino class="card-content" v-bind:tetromino="nextTetromino" />
+        </div>
+        <div class="card hold">
+            <header class="card-header">
+                <p class="card-header-title">
+                    Hold
+                </p>
+            </header>
+            <NextTetromino class="card-content" v-bind:tetromino="holdTetromino" />
+        </div>
+        <div class="card Score">
+            <header class="card-header">
+                <p class="card-header-title">
+                    Score
+                </p>
+            </header>
+            <div class="card-content">
+                {{ score }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import NextTetromino from './NextTetromino.vue'
+import HoldTetromino from './HoldTetromino.vue'
+export default {
+    name: "side",
+    props: ['nextTetromino','holdTetromino', 'score'],
+    components: {
+        NextTetromino,
+        HoldTetromino
+    },
+}
+</script>
+
+<style scoped>
+.card > .card-content {
+    margin: auto;
+}
+</style>
