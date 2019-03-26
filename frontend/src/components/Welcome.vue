2c2a154702eb18a515b5b56bdf1a11575abf5a4d
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2019-03-25 23:57:09 -0700

    update id

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -4,6 +4,30 @@
             Tetris!
         </h1>
         <h4 class="subtitle is-4">Start a new game or join an existing game using given links</h4>
-        <a class="button is-primary is-rounded">Start a new game</a>
+        <a class="button is-primary is-rounded" v-on:click="startNewGame">Start a new game</a>
     </div>
 </template>
+
+<script>
+export default {
+    name: "Welcome",
+    methods: {
+        startNewGame: function() {
+            fetch('//localhost:3000/api/newgame', {
+                method: 'POST',
+                // mode: 'no-cors'
+            }).then(res => res.json())
+            .then((response) => {
+                if (response.success) {
+                    const id = response.id;
+
+                } else {
+                    console.error(response)
+                }
+            }).catch((error) => {
+                console.error(error)
+            })
+        }
+    }
+}
+</script>
