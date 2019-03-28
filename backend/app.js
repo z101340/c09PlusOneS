6719032c1953a73714fde63379814faa751c42f5
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2019-03-28 00:22:08 -0700

    set cookie

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const app = new express();
 
 const cors = require("cors");
 
+const session = require('express-session');
+
 const port = 3000;
 
 const bodyParser = require('body-parser');
@@ -12,27 +14,32 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
-const {MongoClient, ObjectId} = require("mongodb");
+app.use(session({
+    secret: 'keyboard cat',
+}));
+
+const { MongoClient, ObjectId } = require("mongodb");
 const mongoUrl = "mongodb://localhost:27017/tetris";
 
 class Player  {
     constructor() {
         this.matrix = [];
         this.score = 0;
+        this.sid = '';
     }
 }
 
-app.post('/api/newgame', (req, res) => {
-    MongoClient.connect(mongoUrl, function(err, db) {
+app.post('/api/game', (req, res) => {
+    MongoClient.connect(mongoUrl, function (err, db) {
         if (err) {
             console.error(err);
             res.status(500).json(err);
         } else {
             const dbo = db.db("tetris");
             const emptyGame = {
-                starter: new Player(),
-                opponent: new Player(),
-                isFilled: false
+                host: new Player(),
+                guest: new Player(),
+                hostSid: req.sessionID,
             };
 
             dbo.collection("games").insertOne(emptyGame, function(err, dbRes) {
@@ -54,24 +61,37 @@ app.post('/api/newgame', (req, res) => {
     });
 });
 
-app.post('/api/joingame', function (req, res) {
-    const id = req.body.id;
-    MongoClient.connect(mongoUrl, function(err, db) {
+app.post('/api/game/:id', function (req, res) {
+    const id = req.params.id;
+    MongoClient.connect(mongoUrl, function (err, db) {
         if (err) {
             console.log(err);
-            res.status(500).json({success: false, err});
+            res.status(500).json({ success: false, err });
         } else if (!ObjectId.isValid(id)) {
-            res.status(422).json({success: false, err: "bad id input"});
+            res.status(422).json({ success: false, err: "bad id input" });
         } else {
             const dbo = db.db("tetris");
-            dbo.collection("games").updateOne({_id: new ObjectId(id), isFilled: false}, {$set: { isFilled: true}}, function (err, dbRes) {
+            dbo.collection("games").findOne({ _id: new ObjectId(id) }, function (err, dbRes) {
                 if (err) {
                     console.log(err);
-                    res.status(500).json({success: false, err});
-                } else if (dbRes.matchedCount == 1) {
-                    res.json({success: true, result: dbRes.result});
+                    res.status(500).json({ success: false, err });
+                }else if (dbRes) {
+                    let you = {};
+                    let opponent = {};
+                    if (req.sessionID == dbRes.hostSid) {
+                        // client is host
+                        you = dbRes.host;
+                        opponent = dbRes.guest;
+                    } else {
+                        you = dbRes.guest;
+                        opponent = dbRes.host;
+                    }
+                    res.json({ success: true, result: {
+                        you,
+                        opponent
+                    } });
                 } else {
-                    res.status(500).json({success: false, err: "no game matched"});
+                    res.status(500).json({ success: false, err: "no game matched" });
                 }
             });
         }
